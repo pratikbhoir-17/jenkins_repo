b2a9de34865217dc0ea73a2a890d7c64441d2e38
Pratik Bhoir <prateikbhoir17@gmail.com>
2023-10-31 13:01:03 +0530

    we are adding the files for Jenkins job in are newly created Repo

---
+++ src/jenkins_first_project/jenkins/Jenkins_1.java
@@ -0,0 +1,18 @@
+package jenkins_first_project.jenkins;
+
+import java.util.Date;
+
+public class Jenkins_1 {
+
+	public static void main(String[] args) {
+	
+		
+		System.out.println("This is my First Jenkins Java Application");
+		
+		System.out.println("We are building Jenkins Job");
+		
+		System.out.println(new Date());
+
+	}
+
+}
