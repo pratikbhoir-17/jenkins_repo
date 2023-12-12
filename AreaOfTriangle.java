5f0f21e02bcb7a3be1a4b8c242bfd3b694e458e7
Prateik <104353848+pratikbhoir-17@users.noreply.github.com>
2023-12-12 11:24:29 +0530

    Add files via upload

---
+++ AreaOfTriangle.java
@@ -0,0 +1,13 @@
+class AreaOfTriangle 
+{
+   public static void main(String args[]) 
+    {   
+         double b= 10f;
+
+          double h= 5f;
+ 
+                  //Area = (width*height)/2
+      double area=(b*h)/2;
+      System.out.println("Area of Triangle is: " + area);      
+   }
+}
